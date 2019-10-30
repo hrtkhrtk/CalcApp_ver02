2ee667927826a3f74c212a7cd76c7b29a6e1dd52
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-10-30 17:31:00 +0900

    Lesson4課題② ver00_201910301730

--- app/src/main/java/jp/techacademy/hirotaka/iwasaki/calcapp_ver02/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hirotaka/iwasaki/calcapp_ver02/MainActivity.kt
@@ -5,20 +5,44 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         plus_button.setOnClickListener(this)
-        //minus_button.setOnClickListener(this)
-        //multiple_button.setOnClickListener(this)
-        //divide_button.setOnClickListener(this)
+        minus_button.setOnClickListener(this)
+        multiple_button.setOnClickListener(this)
+        divide_button.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onClick(v: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        when(v.id){
+            R.id.plus_button -> {
+                val value = editText_val_01.text
+                intent.putExtra("VALUE", value)
+                Log.d("test191030n10", value.toString())
+            }
+            R.id.minus_button -> {
+                val value = editText_val_01.text
+                intent.putExtra("VALUE", value)
+                Log.d("test191030n11", value.toString())
+            }
+            R.id.multiple_button -> {
+                val value = editText_val_01.text
+                intent.putExtra("VALUE", value)
+                Log.d("test191030n12", value.toString())
+            }
+            R.id.divide_button -> {
+                val value = editText_val_01.text
+                intent.putExtra("VALUE", value)
+                Log.d("test191030n13", value.toString())
+            }
+        }
+        startActivity(intent)
     }
 }
