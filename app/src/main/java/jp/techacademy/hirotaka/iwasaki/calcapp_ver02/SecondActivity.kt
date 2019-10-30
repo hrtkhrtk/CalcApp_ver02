b0aec439f00ab7d61bafeda4492651cd3bb40160
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-10-30 17:26:02 +0900

    Lesson4課題② ver00_201910301725

--- app/src/main/java/jp/techacademy/hirotaka/iwasaki/calcapp_ver02/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/hirotaka/iwasaki/calcapp_ver02/SecondActivity.kt
@@ -2,11 +2,18 @@ package jp.techacademy.hirotaka.iwasaki.calcapp_ver02
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        //val value = intent.getDoubleExtra("VALUE", 0)
+        //val value = intent.getString("VALUE", "0")
+        val value = intent.getIntExtra("VALUE", 0)
+        Log.d("test191030n20", value.toString())
     }
 }
